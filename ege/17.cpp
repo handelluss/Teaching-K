f1964629d1a296bca059d3c6b9f7f582fae445ff
handelluss <91080385+handelluss@users.noreply.github.com>
2022-04-27 21:00:25 +0700

    Create 17.cpp

---
+++ ege/17.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin("/home/handelluss/Desktop/17.txt");
+    int tmp;
+    vector<int> a;
+
+    while (fin >> tmp) {
+        a.push_back(tmp);
+    }
+    /* ДРУГОЙ ВАРИАНТ РЕШЕНИЯ, для этого необходимо посмотреть, сколько в текстовом файле чисел ручками
+        for(int i = 0; i < 5000; i++) {
+            fin >> tmp; a.push_back(tmp);
+        }
+    */
+    int cnt = 0, max_sum = 0;
+    for (int i = 1; i < a.size(); i++) {
+        if (a[i - 1] % 3 == 0 || a[i] % 3 == 0) {
+            cnt++;
+            if (a[i - 1] + a[i] > max_sum)
+                max_sum = a[i - 1] + a[i];
+        }
+    }
+    cout << cnt << " " << max_sum;
+}
