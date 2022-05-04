0d3246d886e6008be6a2e36fff064f1b35a87249
handelluss <91080385+handelluss@users.noreply.github.com>
2022-05-04 21:54:59 +0700

    Update 2995.cpp

--- kompege/17/2995.cpp
+++ kompege/17/2995.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main() {
-    ifstream fin("/home/handelluss/Dev/TeachingCPP/17.txt");
+    ifstream fin("17.txt");
     int tmp;
     vector<int> a;
 
@@ -15,11 +15,11 @@ int main() {
         a.push_back(tmp);
     }
 
-    int sr_ar = accumulate(a.begin(), a.end(), 0) / a.size();
+    int average = accumulate(a.begin(), a.end(), 0) / a.size();
     int counter = 0, max_sum = 0;
 
     for (int i = 0; i < a.size() - 1; i++) {
-        if (a[i] < sr_ar && a[i + 1] < sr_ar && (a[i] % 10 == 9 || a[i + 1] % 10 == 9)) {
+        if (a[i] < average && a[i + 1] < average && (a[i] % 10 == 9 || a[i + 1] % 10 == 9)) {
             counter++;
             if (max_sum < a[i] + a[i + 1]) {
                 max_sum = a[i] + a[i + 1];
